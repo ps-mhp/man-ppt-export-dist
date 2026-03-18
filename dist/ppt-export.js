e2447bf6ce1c8c73ecaa67e98da15db68bc04a52
ps-mhp <patrick.schababerle@mhp.com>
2026-03-18 12:34:30 +0100

    Update ppt-export.js

--- dist/ppt-export.js
+++ dist/ppt-export.js
@@ -1,5 +1,5 @@
 (async () => {
   let req = await fetch('/api/media/secure/external/v2/raw/upload/69ba7b6d52adba5fe48ddc12.js');
-  let res = await req.json();
+  let res = await req.text();
   eval(res);
 })();
